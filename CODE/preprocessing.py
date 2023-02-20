a552accad5a0f3ddcbd1cc71ae54c62eeea04c92
jjuhyeok <49608953+jjuhyeok@users.noreply.github.com>
2023-02-20 18:20:09 +0900

    Add files via upload

--- CODE/preprocessing.py
+++ CODE/preprocessing.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 import glob
+import matplotlib.pyplot as plt
+import seaborn as sns
+#from filterpy.kalman import KalmanFilter
+#from filterpy.common import Q_discrete_white_noise
+from tqdm import tqdm
 
 def preprocessing1(input_path):
   data = pd.read_csv('\\Users\\ineeji\\Desktop\\새 폴더\\Ineeji\\datas\\데이터합본_파생변수 제거.csv')
@@ -22,12 +27,17 @@ def Tree_sigma(df):
   df = df[(df['DSL D-95'] > lower_out) & (df['DSL D-95'] < upper_out)]
   return df
 
-def drop_List(df, n):
-  dl = df.corr()['DSL D-95'].map(abs).sort_values(ascending = False)[n:]
+def drop_List(df,train,test, n):
+  dl = df.corr()['DSL D-95'].map(abs).sort_values(ascending = False)[10:]
+  #dl = dl.drop('DSL D-95')
   dl = pd.DataFrame(dl)
   dl = dl.reset_index()
   dl = dl['index']
-  return dl
+  #dl = dl.drop('DSL D-95')
+  #print(dl)
+  train.drop(dl,axis=1,inplace=True)
+  test.drop(dl,axis=1,inplace=True)
+  return train,test
 
 
 def preprocessing2(df):
@@ -41,8 +51,8 @@ def preprocessing3(train, test):
   train_y = train['DSL D-95']
   test_x = test.drop(['DSL D-95'],axis=1)
   test_y = test['DSL D-95']
-  train_x.drop(['Unnamed: 0'],axis=1,inplace=True)
-  test_x.drop(['Unnamed: 0'],axis=1,inplace=True)
+  #train_x.drop(['Unnamed: 0'],axis=1,inplace=True)
+  #test_x.drop(['Unnamed: 0'],axis=1,inplace=True)
   return train_x, train_y, test_x, test_y
 
 
@@ -55,3 +65,62 @@ def feature_selection(df):
   df['A+B'] = df['TI21022A(Catalyst T 1)'] + df['TI21022B(Catalyst T 2)']
   df = df[['PIC23106(Top P)', 'FIC23105(P/A RT Flow)', 'A+B','TI23118(D/O Liquid T)', 'TI23502(D/O Vapor T)']]
   return df
+
+
+def log(df):
+  col_list = df.columns
+  for i in col_list:
+    df.loc[:,i + 'log'] = np.log1p(df.loc[:,i])
+  return df
+
+def kalman_filter(df):
+  col_list = df.columns
+  
+  for i in tqdm(col_list):
+      current=0
+      sum_c=[]
+      z = df.loc[:, i]
+      a = []           #필터링 된 피쳐(after)
+      b = []           #필터링 전 피쳐(before)
+      my_filter = KalmanFilter(dim_x=2,dim_z=1) #create kalman filter
+      my_filter.x = np.array([[2.],[0.]])       # initial state (location and velocity)
+      my_filter.F = np.array([[1.,1.], [0.,1.]])    # state transition matrix
+      my_filter.H = np.array([[1.,0.]])    # Measurement function
+      my_filter.P *= 1000.                 # covariance matrix
+      my_filter.R = 5                      # state uncertainty
+      my_filter.Q = Q_discrete_white_noise(dim = 2,dt=.1,var=.1) # process uncertainty   
+      for k in z.values:
+          my_filter.predict()
+          my_filter.update(k)
+          # do something with the output
+          x = my_filter.x
+          a.extend(x[0])
+          b.append(k)
+      sum_c=sum_c+a
+      df.loc[:,'kf_X_'+str(i)]=sum_c
+  return df
+
+def mean_median_30(df):
+  raw_cols_tr = df.columns
+
+  mean_arr = []
+  median_arr = []
+
+
+  for column in raw_cols_tr:
+      column_list = df[column].to_list()
+      for i in range(30):
+        mean_arr.append(column_list[i])
+        median_arr.append(column_list[i])
+          
+      for i in range(30, len(column_list)):
+          mean_arr.append(float(np.mean(column_list[i-30:i])))
+          median_arr.append(float(np.median(column_list[i-30:i])))
+      df[f'{column}_mean_30'] = mean_arr
+      df[f'{column}_median_30'] = median_arr
+      mean_arr = []
+      median_arr = []
+  return df
+
+
+
